6a2468118c3b8016a574452eaec1f86d28914466
YuanZheng0125 <yz4929@g.rit.edu>
2020-01-01 20:00:47 -0800

    adjust transition colors and fix hover effect

--- dist/js/work.js
+++ dist/js/work.js
@@ -1,4 +1,3 @@
-
 /**
  * This is for slide toggle effect upon click
  */
@@ -8,14 +7,46 @@ const slideTime = 600;
 
 $(document).ready(function() {
   $(".acc_container").hide();
+
   $("h2.acc_trigger").click(function() {
+    const allSubtitles = $(this)
+      .next(".acc_container")
+      .find("a");
     const toggleSign = $(this).find(".togglePlus");
+
+    if (
+      $(this)
+        .find(".category")
+        .hasClass("active")
+    ) {
+      setTimeout(function() {
+        allSubtitles.removeClass("active");
+        allSubtitles.eq(0).addClass("active");
+      }, 600);
+    } else {
+      allSubtitles.eq(0).addClass("active");
+      $(this)
+        .next(".acc_container")
+        .find("iframe")
+        .attr("src", allSubtitles.eq(0).attr("href"));
+    }
+
     toggleSign.text(toggleSign.text() == "+" ? "-" : "+");
     $(this)
       .find(".category")
       .toggleClass("active");
+
     $(this)
       .next(".acc_container")
       .slideToggle(slideTime);
   });
+
+  $(".projectNavList")
+    .find("a")
+    .click(function() {
+      $(".projectNavList")
+        .find("a")
+        .removeClass("active");
+      $(this).addClass("active");
+    });
 });
